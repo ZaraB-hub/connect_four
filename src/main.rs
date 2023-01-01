69c975868813ef7d685092177e8e97bf6276ba77
Zara <zarabahtanovic@gmail.com>
2023-01-01 18:28:53 +0100

    entry point

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod playagain;
+mod grid;
+mod win;
+mod logic;
+mod custom;// https://stackoverflow.com/questions/48071513/how-to-use-one-module-from-another-module-in-a-rust-cargo-project
+fn main() {
+
+grid::run();
+}
